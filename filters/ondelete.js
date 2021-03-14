e23a3feaab2cab07f6b0adb3cdcf0c69764977c2
thespikedevice <63735286+thespikedevice@users.noreply.github.com>
2021-03-14 09:35:40 -0600

    Update ondelete.js

--- filters/ondelete.js
+++ filters/ondelete.js
@@ -3,10 +3,11 @@ const fs=require("fs")
 
 module.exports=(msg)=>{
   var memdat=fs.readFileSync("./membermsg.json", "utf8")
+  memdat=JSON.parse(memdat)
   try {
     // Why did you delete your message? BAD
     memdat[msg.author.id]-=50
-    fs.writeFileSync("./membermsg.json", "utf8")
+    fs.writeFileSync("./membermsg.json", JSON.stringify(memdat))
   } catch(err) {
     console.log(err)
   }
